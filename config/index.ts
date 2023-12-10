5fa1b50d417ff7e897ac1ea6fb96562ad2d64225
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-12-10 12:57:06 +0800

    Update config

---
+++ config/index.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const baseURL = process.env.NEXT_PUBLIC_API_URL,
+  isServer = typeof window === "undefined";
+
+const api = axios.create({
+  baseURL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export async function get(url: string) {
+  return (await api.get(url)).data;
+}
+
+export async function post<T>(url: string, body: T) {
+  return (await api.post(url, body)).data;
+}
