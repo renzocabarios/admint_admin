8e0f52bbed5979a911b1e67a006f55ec62010448
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-12-11 13:59:59 +0800

    Update events

--- app/events/[id]/page.tsx
+++ app/events/[id]/page.tsx
@@ -1,16 +1,16 @@
 "use client";
 import { get } from "@/config";
 import { parseDate } from "@/utils";
-import { useRouter } from "next/navigation";
+import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const router = useRouter();
+  const params = useParams();
   const [events, setevents] = useState([]);
 
   useEffect(() => {
     const start = async () => {
-      const temp = await get("events");
+      const temp = await get(`participants?find={"event":"${params.id}"}`);
       if (temp.status == "success") setevents(temp.data);
     };
     start();
@@ -21,17 +21,11 @@ export default function Home() {
       <div className="p-3 flex flex-col gap-2">
         {events.map((e: any) => {
           return (
-            <div
-              key={e._id}
-              className="p-2 bg-purple-500 rounded-md"
-              onClick={() => {
-                router.push(`/events/${e._id}`);
-              }}
-            >
-              <p>{e._id}</p>
+            <div key={e._id} className="p-2 bg-purple-500 rounded-md">
               <p>{e.name}</p>
-              <p>{`${parseDate(e.start)}`}</p>
-              <p>{`${parseDate(e.end)}`}</p>
+              <p>{e.email}</p>
+              <p>{e.walletId}</p>
+              <p>Joined: {`${parseDate(e.createdAt)}`}</p>
             </div>
           );
         })}
