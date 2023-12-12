f576669b4519385f122c1b9ced90003f365aa152
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-12-12 17:44:43 +0800

    Add env

---
+++ env/index.ts
@@ -0,0 +1,4 @@
+export const NEXT_PUBLIC_CANDY_MACHINE: string =
+  process.env.NEXT_PUBLIC_CANDY_MACHINE ?? "";
+export const NEXT_PUBLIC_CLUSTER: string =
+  process.env.NEXT_PUBLIC_CLUSTER ?? "";
